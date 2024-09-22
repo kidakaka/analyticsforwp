5e85ae9daf7026851896b3871f095a30ebb6fcc9
Prasad Ajinkya <prasad.ajinkya@gmail.com>
2024-09-22 19:21:57 +0530

    Create index.php
    default GA implementation

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <title>Basic GA setup</title>
+  <head>
+    
+  </head>
+<body>
+<!-- the default method of including GA, this is to be put immediately after the head section  -->
+<?php
+// add your property here. for protecting the property have removed the actual id and added a dummy string
+ $config = "your-propery-id-goes-here";
+?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N89XGQ8QGY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?php echo $config; ?>');
+</script>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
