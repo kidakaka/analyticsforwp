685c5a908bb83fc6b017bf79df34eadceca84cac
Prasad Ajinkya <prasad.ajinkya@gmail.com>
2024-09-22 19:40:16 +0530

    Update event.php
    added instructions and comments

--- event.php
+++ event.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html>
-        <title>Events</title>
-        <head>
+<title>Events</title>
+<head>
+<?php
+// add your property here. for protecting the property have removed the actual id and added a dummy string
+$config = "your-propery-id-goes-here";
+?>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-N89XGQ8QGY"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $config; ?>"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-N89XGQ8QGY');
+  gtag('config', '<?php echo $config; ?>');
 </script>
-        </head>
+</head>
 <body>
 <h1>My First Event</h1>
 
@@ -21,6 +25,8 @@
       *   The following event is sent when the page loads. You could
       *   wrap the event in a JavaScript function so the event is
       *   sent when the user performs some action.
+      *   You can have upto 25 parameters in this event. Therefore you can pretty much add all the major important attributes of the wp_post object
+      *   You could also add additional parameters such as weather at the time of the event, inventory in the ERP for an object, total number of users, etc.
       */
       gtag('event', 'Custom Event', {
         'module': 'Analytics',
